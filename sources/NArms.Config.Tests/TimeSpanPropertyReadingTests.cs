8b046bcd536b8d6055d57725924d5c8f909ab687
eskat0n <muyou.prj@gmail.com>
2014-03-18 12:19:32 +0600

    added test for parsing full time span properties

---
+++ sources/NArms.Config.Tests/TimeSpanPropertyReadingTests.cs
@@ -0,0 +1,30 @@
+﻿namespace NArms.Config.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TimeSpanPropertyReadingTests
+    {
+        private Config _config;
+
+        private class Config : ConfigBase
+        {
+            public TimeSpan FullTimeSpan { get; set; }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _config = new Config();
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadFullTimeSpanValues()
+        {
+            var timeSpan = new TimeSpan(10, 20, 30);
+
+            Assert.AreEqual(timeSpan, _config.FullTimeSpan);
+        }
+    }
+}

ec7c08b5e77d1d695ccd21a88b3381ee38216786
eskat0n <muyou.prj@gmail.com>
2015-03-27 19:00:01 +0500

    implemented getting default value for enum

--- sources/NArms.Config/Deserializers/Default/DefaultEnumDeserializer.cs
+++ sources/NArms.Config/Deserializers/Default/DefaultEnumDeserializer.cs
@@ -4,12 +4,16 @@
 
     internal class DefaultEnumDeserializer : IDeserializer
     {
+        private const string DefaultValueSubstitute = "@Default";
+
         public DefaultEnumDeserializer()
         {
         }
 
         public object Deserialize(Type type, string value)
         {
+            if (value == DefaultValueSubstitute)
+                return Activator.CreateInstance(type);
             return Enum.Parse(type, value);
         }
     }
